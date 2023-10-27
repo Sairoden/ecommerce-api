f1b4e85659c86307aaef22dceb1094196fa9adcc
Sairoden <odingandarosa@gmail.com>
2023-10-27 14:36:08 +0800

    Add security packages

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 // Libraries
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const rateLimiter = require("express-rate-limit");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const cors = require("cors");
+const mongoSanitize = require("express-mongo-sanitize");
 
 // Express
 const express = require("express");
@@ -11,6 +16,17 @@ const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
 // Middleware
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 60,
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(mongoSanitize());
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
@@ -27,4 +43,4 @@ app.use(errorHandlerMiddleware);
 
 module.exports = app;
 
-// 66 ka na
+// 69 ka na
